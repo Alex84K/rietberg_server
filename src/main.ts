b6b213a49a1e32dc83be16fe38b292d5b3558c43
Alex <al.k.84.de@gmail.com>
2026-06-25 20:22:19 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ async function bootstrap() {
   app.enableCors({
     origin: isDev
       ? (origin, callback) => callback(null, true)
-      : ['https://rietberg.de', 'https://www.rietberg.de', 'http://localhost:5173'],
+      : ['https://rietberg-cbg-serv.smartsoftsystem.de', 'http://localhost:5173'],
     credentials: true,
   });
 
